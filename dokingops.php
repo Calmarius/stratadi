24edc7bba7712534b94c8e109d52d86368ea1001
Calmarius <calmarius@calmarius.net>
2015-06-07 17:38:56 +0200

    Modernized kingops.

--- dokingops.php
+++ dokingops.php
@@ -5,45 +5,47 @@ bounceSessionOver();
 
 if (!isset($_GET['id'])) $_GET['id']=$_SESSION['accessId'];
 
-$r=doMySqlQuery(sqlPrintf("SELECT * FROM wtfb2_accesses WHERE (id='{1}')",array($_GET['id'])));
-if (mysql_num_rows($r)==0)
+// TODO: Never use $_GET for operations!
+
+$r=runEscapedQuery("SELECT * FROM wtfb2_accesses WHERE (id={0})", $_GET['id']);
+if (isEmptyResult($r))
 {
 	jumpErrorPage($language['usernamenotexist']);
 }
-$access=mysql_fetch_assoc($r);
+$access=$r[0][0];
 
 if ($_GET['cmd']=='kick')
 {
 	if (!imAccountMaster()) jumpErrorPage($language['accessdenied']);
 	if ($_GET['id']==$_SESSION['accessId']) jumpErrorPage($language['youmustgiveawaymasteraccessfirst']);
-	$r=doMySqlQuery(sqlPrintf("SELECT COUNT(*) AS cnt FROM wtfb2_accesses WHERE (accountId='{1}')",array($_SESSION['userId'])));
-	$a=mysql_fetch_assoc($r);
+	$r=runEscapedQuery("SELECT COUNT(*) AS cnt FROM wtfb2_accesses WHERE (accountId={0})",$_SESSION['userId']);
+	$a=$r[0][0];
 	if ($a['cnt']<=1) jumpErrorPage($language['cantkicklastking']);
-	doMySqlQuery(sqlPrintf("UPDATE wtfb2_accesses SET accountId=0 WHERE (id='{1}')",array($_GET['id'])));
-	doMySqlQuery(sqlPrintf("INSERT INTO wtfb2_worldevents (eventTime,type,recipientId) VALUES (NOW(),'forcelogout','{1}')",array($_SESSION['userId'])));
+	runEscapedQuery("UPDATE wtfb2_accesses SET accountId=0 WHERE (id={0})",$_GET['id']);
+	runEscapedQuery("INSERT INTO wtfb2_worldevents (eventTime,type,recipientId) VALUES (NOW(),'forcelogout',{0})",$_SESSION['userId']);
 }
 if ($_GET['cmd']=='setmaster')
 {
 	if (!imAccountMaster()) jumpErrorPage($language['accessdenied']);
-	doMySqlQuery(sqlPrintf("UPDATE wtfb2_users SET masterAccess='{2}' WHERE (id='{1}')",array($_SESSION['userId'],$_GET['id'])));
+	runEscapedQuery("UPDATE wtfb2_users SET masterAccess={1} WHERE (id={0})",$_SESSION['userId'],$_GET['id']);
 }
 if ($_GET['cmd']=='leave')
 {
 	if (imAccountMaster()) jumpErrorPage($language['youmustgiveawaymasteraccessfirst']);
-	$r=doMySqlQuery(sqlPrintf("SELECT COUNT(*) AS cnt FROM wtfb2_accesses WHERE (accountId='{1}')",array($_SESSION['userId'])));
-	$a=mysql_fetch_assoc($r);
+	$r=runEscapedQuery("SELECT COUNT(*) AS cnt FROM wtfb2_accesses WHERE (accountId={0})",$_SESSION['userId']);
+	$a=$r[0][0];
 	if ($a['cnt']<=1) jumpErrorPage($language['cantkicklastking']);
-	doMySqlQuery(sqlPrintf("UPDATE wtfb2_accesses SET accountId=0 WHERE (id='{1}')",array($_SESSION['accessId'])));
+	runEscapedQuery("UPDATE wtfb2_accesses SET accountId=0 WHERE (id={0})",$_SESSION['accessId']);
 	jumpTo('doreset.php');
 }
 if ($_GET['cmd']=='newking')
 {
-	$r=doMySqlQuery(sqlPrintf("SELECT * FROM wtfb2_accesses WHERE (userName='{1}')",array($_POST['newking'])));
-	if (mysql_num_rows($r)<=0) jumpErrorPage($language['usernamenotexist']);
-	$a=mysql_fetch_assoc($r);
-	$r=doMySqlQuery(sqlPrintf("SELECT * FROM wtfb2_users WHERE (id='{1}')",array($a['accountId'])));
-	if (mysql_num_rows($r)>0) jumpErrorPage($language['thiskingisalreadycontrollingakingdom']);
-	doMySqlQuery(sqlPrintf("UPDATE wtfb2_accesses SET accountId='{2}' WHERE (id='{1}')",array($a['id'],$_SESSION['userId'])));
+	$r=runEscapedQuery("SELECT * FROM wtfb2_accesses WHERE (userName={0})",$_POST['newking']);
+	if (isEmptyResult($r)) jumpErrorPage($language['usernamenotexist']);
+	$a=$r[0][0];
+	$r=runEscapedQuery("SELECT * FROM wtfb2_users WHERE (id={0})",$a['accountId']);
+	if (!isEmptyResult($r)) jumpErrorPage($language['thiskingisalreadycontrollingakingdom']);
+	runEscapedQuery("UPDATE wtfb2_accesses SET accountId={1} WHERE (id={0})",$a['id'],$_SESSION['userId']);
 }
 
 jumpTo('editkings.php');
