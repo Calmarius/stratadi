dcfab1b0793e8e85b927a3071b94b0408cde6a95
Calmarius <calmarius@calmarius.net>
2015-06-12 09:21:18 +0200

    Modernized profile.php

--- profile.php
+++ profile.php
@@ -4,16 +4,15 @@ require_once('userworkerphps.php');
 
 if (!isset($_SESSION['userId'])) jumpErrorPage($language['sessionisover']);
 
-$q=
-sqlPrintf(
+$r=
+runEscapedQuery(
 	"
 		SELECT wtfb2_users.*,wtfb2_heroes.avatarLink AS heroAvatar,wtfb2_heroes.name AS heroName,wtfb2_heroes.id AS heroId
 		FROM wtfb2_users
 		LEFT JOIN wtfb2_heroes ON (wtfb2_heroes.ownerId=wtfb2_users.id )
-		WHERE (wtfb2_users.id='{1}')
-	",array($_SESSION['userId']));
-$r=doMySqlQuery($q,'jumpErrorPage');
-$a=mysql_fetch_assoc($r);
+		WHERE (wtfb2_users.id={0})
+	",$_SESSION['userId']);
+$a=$r[0][0];
 
 if ($a['heroId']=='')
 {
