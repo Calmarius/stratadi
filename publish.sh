46a997b3a121e4e1eb9850965ce8eb4566b60b10
Calmarius <calmarius@calmarius.net>
2015-06-15 09:54:10 +0200

    .htaccess php settings doesn't work.
    Modified publish.sh to upload hidden files too.

--- publish.sh
+++ publish.sh
@@ -4,10 +4,12 @@ function publish()
 {
     HOST=$1
     DIR=$2
-    
+
     echo "Publishing on host $HOST into directory $DIR"
-    
+
+    shopt -s dotglob
     ncftpput -R -v -u $USERNAME -p $PASSWORD $HOST $DIR export/*
+    shopt -u dotglob
 }
 
 read -p "Enter username: " USERNAME
