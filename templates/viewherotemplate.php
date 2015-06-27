21a76d11d77df92d380a00a459ddf01c62c442cc
Calmarius <calmarius@calmarius.net>
2015-06-27 09:37:44 +0200

    Hero text now includes village coordinates too.

--- templates/viewherotemplate.php
+++ templates/viewherotemplate.php
@@ -10,5 +10,12 @@ global $language;
 <p class="center"><img src="<?php echo $this->hero['avatarLink']; ?>" alt="<?php echo $language['heropicture']; ?>"></p>
 <p class="center"><?php echo xprintf($language['heroattackskill'],array($this->hero['attackskill'],$this->hero['attacknextxp'],$this->hero['offense']));?></p>
 <p class="center"><?php echo xprintf($language['herodefendskill'],array($this->hero['defendskill'],$this->hero['defendnextxp'],$this->hero['defense']));?></p>
-<p class="center"><?php echo xprintf($language['yourheroinvillage'],array('<a href="javascript:void(parent.initMap('.(int)$this->hero['villageX'].','.(int)$this->hero['villageY'].'))">'.$this->hero['villageName'].'</a>'));?></p>
+<p class="center">
+    <?php
+        echo xprintf($language['yourheroinvillage'],
+            array('<a href="javascript:void(parent.initMap('.(int)$this->hero['villageX'].','.(int)$this->hero['villageY'].'))">'.
+                xprintf($language['villagetext'], array($this->hero['villageName'], $this->hero['villageX'], $this->hero['villageY'])).
+            '</a>'));
+    ?>
+</p>
 
