2ba66542e02a12e0c3791d04119ae241161d7746
Calmarius <calmarius@calmarius.net>
2015-07-11 13:00:22 +0200

    Moved tasks to their own file.

--- eventprocessor.php
+++ eventprocessor.php
@@ -7,17 +7,6 @@ require_once("battlecalculation.php");
 require_once("templateclass.php");
 require_once("nightbonus.php");
 
-// TODO: (UI) Do not show buildings to attack when "moving troops".
-// TODO: (UI) Prompt for confirmation when sending hero in a "last man standing battle"
-// TODO: (wishlist) Show other things like night bonus and age bonus is battle report to be able to reproduce the conditions in the battle simulator.
-// TODO: (wishlist) Make hero attack and defense levels public.
-// TODO: (wishlist) Check the 3:1 rule when the troops arrive.
-// TODO: (task) Make simple login.
-// TODO: (task) Remove iwiw sharing.
-// TODO: (security, epic): Remove all doMySqlQuery and replace it with the mysqli runEscapedQuery. - WIP
-// TODO: (wishlist) Make numbers grouped by thousands.
-// TODO: (UI) When player is not in a guild the guild page is shifted down.
-
 //logText("eventProcessor got called!");
 
 $events=array();
---
+++ todos.php
@@ -0,0 +1,18 @@
+<?php
+/*
+// TODO: (UI) Do not show buildings to attack when "moving troops".
+// TODO: (UI) Prompt for confirmation when sending hero in a "last man standing battle"
+// TODO: (wishlist) Show other things like night bonus and age bonus is battle report to be able to reproduce the conditions in the battle simulator.
+// TODO: (wishlist) Make hero attack and defense levels public.
+// TODO: (wishlist) Check the 3:1 rule when the troops arrive.
+// TODO: (task) Make simple login.
+// TODO: (task) Remove iwiw sharing.
+// TODO: (security, epic): Remove all doMySqlQuery and replace it with the mysqli runEscapedQuery. - WIP
+// TODO: (wishlist) Make numbers grouped by thousands.
+// TODO: (UI) When player is not in a guild the guild page is shifted down.
+// TODO: (wishlist): Make forum not link to something external, instead use the regular threads to make an internal forum.
+
+*/
+
+
+?>
