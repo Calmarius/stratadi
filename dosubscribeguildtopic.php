60da9a96a8eb1c4a6ba9ed9a3041637c2ee26686
Calmarius <calmarius@calmarius.net>
2015-07-11 12:44:58 +0200

    Modernized SQL in doSubsubscribeguildtopic.php.

--- dosubscribeguildtopic.php
+++ dosubscribeguildtopic.php
@@ -4,15 +4,15 @@ require_once('userworkerphps.php');
 bounceSessionOver();
 $myId=$_SESSION['userId'];
 $threadId=$_GET['id'];
-$r=doMySqlQuery(sqlPrintf("SELECT * FROM wtfb2_users WHERE (id='{1}')",array($myId)),'jumpErrorPage');
-if (mysql_num_rows($r)==0) jumpErrorPage($language['accessdenied']);
-$myself=mysql_fetch_assoc($r);
+$r=runEscapedQuery("SELECT * FROM wtfb2_users WHERE (id={0})",$myId);
+if (isEmptyResult($r)) jumpErrorPage($language['accessdenied']);
+$myself=$r[0][0];
 
-$r=doMySqlQuery(sqlPrintf("SELECT * FROM wtfb2_threads WHERE (id='{1}') AND (guildId='{2}')",array($threadId,$myself['guildId'])),'jumpErrorPage');
-if (mysql_num_rows($r)==0) jumpErrorPage($language['accessdenied']);
-$thread=mysql_fetch_assoc($r);
+$r=runEscapedQuery("SELECT * FROM wtfb2_threads WHERE (id={0}) AND (guildId={1})",$threadId,$myself['guildId']);
+if (isEmptyResult($r)) jumpErrorPage($language['accessdenied']);
+$thread=$r[0][0];
 
-doMySqlQuery(sqlPrintf("INSERT INTO wtfb2_threadlinks (threadId,userId,`read`) VALUES ('{1}','{2}',0)",array($threadId,$myId)),'jumpErrorPage');
+runEscapedQuery("INSERT INTO wtfb2_threadlinks (threadId,userId,`read`) VALUES ({0},{1},0)",$threadId,$myId);
 
 jumpTo("messages.php");
 
