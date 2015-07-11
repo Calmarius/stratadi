ae9b0f253b7552710975682016a2ae265f3373e0
Calmarius <calmarius@calmarius.net>
2015-07-11 12:36:42 +0200

    Modernized SQL in dodeleteme.php.

--- dodeleteme.php
+++ dodeleteme.php
@@ -4,7 +4,7 @@ require_once('userworkerphps.php');
 
 if (isset($_SESSION['asdeputy']) && $_SESSION['asdeputy']) jumpErrorPage($language['accessdenied']);
 
-doMySqlQuery(sqlPrintf("UPDATE wtfb2_users SET willDeleteAt=DATE_ADD(NOW(),INTERVAL 2 WEEK) WHERE (id='{1}')",array($_SESSION['userId'])));
+runEscapedQuery("UPDATE wtfb2_users SET willDeleteAt=DATE_ADD(NOW(),INTERVAL 2 WEEK) WHERE (id={0})",$_SESSION['userId']);
 
 jumpTo('doreset.php');
 
