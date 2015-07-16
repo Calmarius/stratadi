0bf05ee01c885d36889029559375d29a5a1d660b
Calmarius <calmarius@calmarius.net>
2015-07-16 23:30:57 +0200

    Modernized SQL in dokick.php

--- dokick.php
+++ dokick.php
@@ -4,26 +4,33 @@ require_once('userworkerphps.php');
 
 bounceSessionOver();
 $myId=$_SESSION['userId'];
-$r=doMySqlQuery(sqlPrintf("SELECT * FROM wtfb2_guildpermissions WHERE (userId='{1}') AND (permission='kick')",array($myId)),'jumpErrorPage');
-if (mysql_num_rows($r)==0) jumpErrorPage($language['accessdenied']);
-$r=doMySqlQuery(sqlPrintf("SELECT * FROM wtfb2_users WHERE (id='{1}')",array($myId)),'jumpErrorPage');
-$me=mysql_fetch_assoc($r);
-$r=doMySqlQuery(sqlPrintf("SELECT * FROM wtfb2_guilds WHERE (id='{1}')",array($me['guildId'])),'jumpErrorPage');
-$guild=mysql_fetch_assoc($r);
+$r=runEscapedQuery("SELECT * FROM wtfb2_guildpermissions WHERE (userId={0}) AND (permission='kick')",$myId);
+if (isEmptyResult($r)) jumpErrorPage($language['accessdenied']);
+$r=runEscapedQuery("SELECT * FROM wtfb2_users WHERE (id={0})",$myId);
+$me=$r[0][0];
+$r=runEscapedQuery("SELECT * FROM wtfb2_guilds WHERE (id={0})",$me['guildId']);
+$guild=$r[0][0];
 
 $kickeeId=(int)$_GET['id'];
-$r=doMySqlQuery(sqlPrintf("SELECT * FROM wtfb2_users WHERE (id='{1}')",array($kickeeId)),'jumpErrorPage');
-$kickee=mysql_fetch_assoc($r);
-doMySqlQuery(sqlPrintf("INSERT INTO wtfb2_worldevents (eventTime,playerId,type) VALUES (NOW(),'{1}','guildchange')",array($kickeeId)));	 // guild change
-doMySqlQuery(sqlPrintf("INSERT INTO wtfb2_worldevents (eventTime,guildId,type,recipientId,needFullRefresh) VALUES (NOW(),'{1}','diplomacychange','{2}',1)"),array($me['guildId'],$kickeeId)); // notify the player too.
+$r=runEscapedQuery("SELECT * FROM wtfb2_users WHERE (id={0})",$kickeeId);
+$kickee=$r[0][0];
+runEscapedQuery("INSERT INTO wtfb2_worldevents (eventTime,playerId,type) VALUES (NOW(),{0},'guildchange')",$kickeeId);	 // guild change
+runEscapedQuery("
+    INSERT INTO wtfb2_worldevents
+    (eventTime,guildId,type,recipientId,needFullRefresh)
+    VALUES
+    (NOW(),{0},'diplomacychange',{1},1)",
+    $me['guildId'],
+    $kickeeId
+); // notify the player too.
 // kick from the guild
-doMySqlQuery(sqlPrintf("UPDATE wtfb2_users SET guildId=NULL WHERE (id='{1}')",array($kickeeId)));
+runEscapedQuery("UPDATE wtfb2_users SET guildId=NULL WHERE (id={0})",$kickeeId);
 // revoke his rights
-doMySqlQuery(sqlPrintf("DELETE FROM wtfb2_guildpermissions WHERE (userId='{1}')",array($kickeeId)));
+runEscapedQuery("DELETE FROM wtfb2_guildpermissions WHERE (userId={0})",$kickeeId);
 // send him a farewell report
 $repTitle=$language['youhavebeenkicked'];
 $repContent=xprintf($language['someonekickedyoufromthesomeguild'],array($myId,$me['userName'],$guild['id'],$guild['guildName']));
-doMySqlQuery(sqlPrintf("INSERT INTO wtfb2_reports (recipientId,title,text,reportTime,token) VALUES ('{1}','{2}','{3}',NOW(),MD5(RAND()))",array($kickeeId,$repTitle,$repContent)));
+runEscapedQuery("INSERT INTO wtfb2_reports (recipientId,title,text,reportTime,token) VALUES ({0},{1},{2},NOW(),MD5(RAND()))",$kickeeId,$repTitle,$repContent);
 // well done
 jumpSuccessPage(xprintf($language['playerhasbeenkicked'],array($kickeeId,$kickee['userName'])),'');
 
