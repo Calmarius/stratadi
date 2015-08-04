1d8716e802615fcfa8861685bfe588381a39af49
Calmarius <calmarius@calmarius.net>
2015-08-04 23:44:01 +0200

    Modernized SQL in viewreport.php

--- viewreport.php
+++ viewreport.php
@@ -6,43 +6,49 @@ $myId=@$_SESSION['userId'];
 $reportId=(int)$_GET['id'];
 $token=$_GET['token'];
 
-$r=doMySqlQuery(sqlPrintf("SELECT * FROM wtfb2_reports WHERE (id='{1}') AND (token='{2}') AND ((recipientId='$myId') OR (isPublic=1))",array($reportId,$token)),'jumpErrorPage');
-if (mysql_num_rows($r)==0) jumpErrorPage($language['accessdenied']);
-$report=mysql_fetch_assoc($r);
+$r=runEscapedQuery("SELECT * FROM wtfb2_reports WHERE (id={0}) AND (token={1}) AND ((recipientId='$myId') OR (isPublic=1))",$reportId,$token);
+if (isEmptyResult($r)) jumpErrorPage($language['accessdenied']);
+$report=$r[0][0];
 
-doMySqlQuery(sqlPrintf("UPDATE wtfb2_reports SET isRead=1 WHERE (id='{1}')",array($reportId)),'jumpErrorPage');
+runEscapedQuery("UPDATE wtfb2_reports SET isRead=1 WHERE (id={0})",$reportId);
 
 $showOptions=$report['recipientId']==$myId;
 
 // find next and previous id.
 
-$r=doMySqlQuery(sqlPrintf("SELECT MIN(id) AS id FROM wtfb2_reports WHERE (id>'{1}') AND ((recipientId='$myId'))",array($reportId)));
-if (mysql_num_rows($r)>0)
+$r=runEscapedQuery("SELECT MIN(id) AS id FROM wtfb2_reports WHERE (id>{0}) AND ((recipientId='$myId'))",$reportId);
+if (!isEmptyResult($r))
 {
-	$a=mysql_fetch_assoc($r);
+	$a=$r[0][0];
 	$report['nextId']=$a['id'];
-	$r=doMySqlQuery(sqlPrintf("SELECT token FROM wtfb2_reports WHERE (id='{1}')",array($a['id'])));
-	$a=mysql_fetch_assoc($r);
-	$report['nextToken']=$a['token'];
+	$r=runEscapedQuery("SELECT token FROM wtfb2_reports WHERE (id={0})",$a['id']);
+	if (!isEmptyResult($r))
+	{
+	    $a=$r[0][0];
+	    $report['nextToken']=$a['token'];
+	}
 }
 
 if (isset($_GET['op']) && ($_GET['op']=='delete'))
 {
-	doMySqlQuery(sqlPrintf("DELETE FROM wtfb2_reports WHERE (id='{1}')",array($reportId)));
+	runEscapedQuery("DELETE FROM wtfb2_reports WHERE (id={0})",$reportId);
 	if (isset($report['nextId']))
 		jumpTo('viewreport.php?id='.$report['nextId'].'&token='.$report['nextToken']);
 	else
 		jumpTo('reports.php');
 }
 
-$r=doMySqlQuery(sqlPrintf("SELECT MAX(id) AS id FROM wtfb2_reports WHERE (id<'{1}') AND (recipientId='$myId')",array($reportId)));
-if (mysql_num_rows($r)>0)
+$r=runEscapedQuery("SELECT MAX(id) AS id FROM wtfb2_reports WHERE (id<{0}) AND (recipientId='$myId')",$reportId);
+if (!isEmptyResult($r))
 {
-	$a=mysql_fetch_assoc($r);
+	$a=$r[0][0];
 	$report['prevId']=$a['id'];
-	$r=doMySqlQuery(sqlPrintf("SELECT token FROM wtfb2_reports WHERE (id='{1}')",array($a['id'])));
-	$a=mysql_fetch_assoc($r);
-	$report['prevToken']=$a['token'];
+	$r=runEscapedQuery("SELECT token FROM wtfb2_reports WHERE (id={0})",$a['id']);
+	if (!isEmptyResult($r))
+	{
+	    $a=$r[0][0];
+	    $report['prevToken']=$a['token'];
+	}
 }
 
 $link='http://'.$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'].'?'."id=".$report['id']."&token=".$report['token'];
