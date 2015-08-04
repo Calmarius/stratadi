ca88fc72de14200d96c8ff6870932346a19a42ed
Calmarius <calmarius@calmarius.net>
2015-08-04 23:35:03 +0200

    Modernized SQL in dorevokeguildinvitation.php

--- dorevokeguildinvitation.php
+++ dorevokeguildinvitation.php
@@ -4,31 +4,25 @@ require_once("userworkerphps.php");
 
 bounceSessionOver();
 
-$r=doMySqlQuery(sqlPrintf("SELECT * FROM wtfb2_users WHERE (id='{1}')",array($_SESSION['userId'])));
-$me=mysql_fetch_assoc($r);
+$r=runEscapedQuery("SELECT * FROM wtfb2_users WHERE (id={0})",$_SESSION['userId']);
+$me=$r[0][0];
 
-$r=doMySqlQuery(sqlPrintf("SELECT * FROM wtfb2_guildpermissions WHERE (userId='{1}') AND (permission='invite')",array($userId)),'jumpErrorPage');
-if (mysql_num_rows($r)==0) jumpErrorPage($language['accessdenied']);
+$r=runEscapedQuery("SELECT * FROM wtfb2_guildpermissions WHERE (userId={0}) AND (permission='invite')",$_SESSION['userId']);
+if (isEmptyResult($r)) jumpErrorPage($language['accessdenied']);
 
-$r=doMySqlQuery(sqlPrintf("SELECT i.*,g.guildName FROM wtfb2_guildinvitations i JOIN wtfb2_guilds g ON (i.guildId=g.id) WHERE (i.id='{1}') AND (i.guildId='{2}')",array($_GET['id'],$me['guildId'])),'jumpErrorPage');
-if (mysql_num_rows($r)==0) jumpErrorPage($language['accessdenied']);
-$invitation=mysql_fetch_assoc($r);
+$r=runEscapedQuery("SELECT i.*,g.guildName FROM wtfb2_guildinvitations i JOIN wtfb2_guilds g ON (i.guildId=g.id) WHERE (i.id={0}) AND (i.guildId={1})",$_GET['id'],$me['guildId']);
+if (isEmptyResult($r)) jumpErrorPage($language['accessdenied']);
+$invitation=$r[0][0];
 
 // delete the invitation
-doMySqlQuery(sqlPrintf("DELETE FROM wtfb2_guildinvitations WHERE (id='{1}')",array($_GET['id'])));
+runEscapedQuery("DELETE FROM wtfb2_guildinvitations WHERE (id={0})",$_GET['id']);
 // send report to the recipient
-doMySqlQuery
+runEscapedQuery
 (
-	sqlPrintf
-	(
-		"INSERT INTO wtfb2_reports (recipientId,title,text,reportTime,reportType) VALUES ('{1}','{2}','{3}',NOW(),'unknown')",
-		array
-		(
-			$invitation['recipientId'],
-			$language['revokeinvitationtitle'],
-			xprintf($language['revokeinvitationtext'],array('<a href="viewguild.php?id='.$invitation['guildId'].'">'.$invitation['guildName'].'</a>'))
-		)
-	)
+	"INSERT INTO wtfb2_reports (recipientId,title,text,reportTime,reportType) VALUES ({0},{1},{2},NOW(),'unknown')",
+	$invitation['recipientId'],
+	$language['revokeinvitationtitle'],
+	xprintf($language['revokeinvitationtext'],array('<a href="viewguild.php?id='.$invitation['guildId'].'">'.$invitation['guildName'].'</a>'))
 );
 
 jumpTo('guildops.php?cmd=invite');
